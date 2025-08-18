984ffa1a32fe18a95ee6f1b0b4cdb8d7cd35b115
Sana0523 <sanafathimanadaf23@gmail.com>
2025-08-18 23:33:41 +0530

    added description

--- Leetcode/stacks/maximal_rectangle.cpp
+++ Leetcode/stacks/maximal_rectangle.cpp
@@ -1,3 +1,8 @@
+// LeetCode 85: Maximal Rectangle
+// Problem: https://leetcode.com/problems/maximal-rectangle/
+// Given a 2D binary matrix filled with 0's and 1's, find the largest rectangle containing only 1's and return its area.
+// The solution builds on the concept of the largest rectangle in a histogram. It first constructs a prefix sum matrix where each cell contains the height of consecutive '1's up to that point. Then, it applies the largest rectangle area calculation for each row of this prefix sum matrix, treating each row as a histogram.
+
 class Solution {
 public:
     int maxhist(vector<int>& heights)
