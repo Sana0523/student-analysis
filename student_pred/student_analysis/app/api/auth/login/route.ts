eb1d92b9ade524fd0a76e32bea6d5ce6eaa685ed
Sana0523 <sanafathimanadaf23@gmail.com>
2025-08-21 01:07:17 +0530

    added basic auth

--- student_pred/student_analysis/app/api/auth/login/route.ts
+++ student_pred/student_analysis/app/api/auth/login/route.ts
@@ -1,28 +1,33 @@
-import { NextResponse } from "next/server";
-import { users } from "@/app/lib/mockData";
-export async function POST(request: Request) {
-  const body=await request.json();
-  const { email, password } = body;
-  // Extracted data from response
-  
-  const user =users.find(
-    (u) => u.email==email && u.password==password 
-  );
-  if(user){
-    const userWithoutPasswrod={
-      id:user.id,
-      email:user.email,
-      role:user.role
-    };
-    return NextResponse.json
-    ({success: true,
-      token: "fake-token-123456",
-      user: userWithoutPasswrod});
-  }
-  else{
-    return NextResponse.json(
-      {success: false, message: "Invalid credentials"},
-      { status: 401 }
+  import { NextResponse } from "next/server";
+  import { users } from "@/app/lib/mockData";
+  import jwt from 'jsonwebtoken';
+
+  const SECRET_KEY='my key';
+  export async function POST(request: Request) {
+    const body=await request.json();
+    const { email, password } = body;
+    // Extracted data from response
+    
+    const user =users.find(
+      (u) => u.email==email && u.password==password 
     );
-  }
-  }
+    if(user){
+      const userWithoutPasswrod={
+        id:user.id,
+        email:user.email,
+        role:user.role
+      };
+      const accessToken=jwt.sign(userPayload, SECRET_KEY, { expiresIn: '1h' });
+
+      return NextResponse.json
+      ({success: true,
+        token: accessToken,
+        user: userPayload});
+    }
+    else{
+      return NextResponse.json(
+        {success: false, message: "Invalid credentials"},
+        { status: 401 }
+      );
+    }
+    }
